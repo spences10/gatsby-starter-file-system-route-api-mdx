145a712b3c1126a9503ecd2868e341d02dfb1f1b
spences10 <spences10apps@gmail.com>
2020-11-30 09:05:28 +0000

    change theme

--- src/components/code.js
+++ src/components/code.js
@@ -1,6 +1,6 @@
 import { Flex, Grid, Spacer } from '@chakra-ui/react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
-import theme from 'prism-react-renderer/themes/vsDark'
+import theme from 'prism-react-renderer/themes/nightOwl'
 import React from 'react'
 import {
   BashSvg,
