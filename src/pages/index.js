6dc3dcdc8d66e651e1959d05e7a5840d255bed73
spences10 <spences10apps@gmail.com>
2020-12-01 09:09:43 +0000

    use gatsby graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import { Box, Heading, Link, Text } from '@chakra-ui/react'
-import { Link as GatsbyLink } from 'gatsby'
+import { graphql, Link as GatsbyLink } from 'gatsby'
 import React from 'react'
 import SEO from '../components/seo'
 
